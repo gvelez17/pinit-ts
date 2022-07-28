685e7720ba39927b27d9b433630d677d8d64de8a
Golda Velez <golda@3box.io>
2022-07-28 17:28:09 +0100

    starting to add tags

---
+++ src/commands/message/tag-msg.ts
@@ -0,0 +1,41 @@
+import {
+    ApplicationCommandOptionType,
+    ApplicationCommandType,
+    RESTPostAPIContextMenuApplicationCommandsJSONBody,
+} from 'discord-api-types/v10';
+import { Message, MessageContextMenuInteraction, PermissionString } from 'discord.js';
+import { RateLimiter } from 'discord.js-rate-limiter';
+import { DateTime } from 'luxon';
+
+//import { addTag } from '../../use-composites/add_tag.js'
+import { EventData } from '../../models/internal-models.js';
+import { Lang } from '../../services/index.js';
+import { InteractionUtils } from '../../utils/index.js';
+import { Command, CommandDeferType } from '../index.js';
+
+export class AddTag implements Command {
+    public metadata: RESTPostAPIContextMenuApplicationCommandsJSONBody = {
+        type: ApplicationCommandType.Message,
+        name: Lang.getCom('messageCommands.addTag'),
+        default_member_permissions: undefined,
+        dm_permission: true,
+    };
+    public cooldown = new RateLimiter(1, 5000);
+    public deferType = CommandDeferType.PUBLIC;
+    public requireClientPerms: PermissionString[] = [];
+
+    public async execute(intr: MessageContextMenuInteraction, data: EventData): Promise<void> {
+        const msg = intr.options.data[0].message
+        const what = msg.content
+        const curator = null; // current logged in user, not author
+        const who = `${msg.author.username}#${msg.author.discriminator}` 
+
+//        const res = await addTag(what, who)
+//        console.log(JSON.stringify(res))
+        await InteractionUtils.send(
+            intr,
+            Lang.getEmbed('displayEmbeds.addTag', data.lang(), {
+            })
+        );
+    }
+}
